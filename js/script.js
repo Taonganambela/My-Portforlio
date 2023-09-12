ad6be92eaf14b4bf93b65a8d1de9b94658c2cc3c
Taonga Nambela <taonganambela@mail.com>
2023-09-13 00:30:17 +0200

    removed coments from js

--- js/script.js
+++ js/script.js
@@ -46,37 +46,3 @@ validateMail(() => {
     }
 })
 
-// showError(input, message) = () => {
-//     const formControl = input.parentElement;
-//     formControl.className = 'form-control error';
-//     const small = formControl.querySelector('small');
-//     small.innerText = message;
-//   }
-  
-//   // Show success outline
-// showSuccess(input) = () => {
-//     const formControl = input.parentElement;
-//     formControl.className = 'form-control success';
-//   }
-
-
-// function sendEmail() {
-
-//     Email.send({
-//       Host: "smtp.gmail.com",
-//       Username: "sender@email_address.com",
-//       Password: "Enter your password",
-//       To: 'receiver@email_address.com',
-//       From: "sender@email_address.com",
-//       Subject: "Sending Email using javascript",
-//       Body: "Well that was easy!!",
-//     })
-//       .then(function (message) {
-//         alert("mail sent successfully")
-        
-//       });
-//   }
-
-
-
-
